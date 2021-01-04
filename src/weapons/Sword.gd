d8b1b7d4509a69a8439d921a1bafcb5a913f3e1c
abeardevil <c@lebcameron.com>
2021-01-04 11:28:09 -0800

    Look mom I did math

--- src/weapons/Sword.gd
+++ src/weapons/Sword.gd
@@ -1,9 +1,28 @@
 extends Weapon
 
+var draw_radius := 20
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	weaponType = weaponTypes.SWORD
 
+func _process(delta):
+	if is_instance_valid(weaponOwner):
+		var mousepos: Vector2 = get_global_mouse_position()
+		var pos: Vector2 = weaponOwner.get_ref().get_position()
+		
+		var delta_x = mousepos.x - pos.x
+		var delta_y = pos.y - mousepos.y
+		
+		rotation = atan2(delta_y, delta_x) * -1
+		
+		var finalPos := Vector2.ZERO
+		
+		finalPos.x = pos.x + cos(rotation)*draw_radius
+		finalPos.y = pos.y + sin(rotation)*draw_radius
+		
+		set_position(finalPos)
+		
 
 func _on_Sword_body_entered(body):
 	print("Body entered: " + body.name)
